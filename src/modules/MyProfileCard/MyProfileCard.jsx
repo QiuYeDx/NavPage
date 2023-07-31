7ce35753aea82b5a62e0aca51fd5deb9287b3fb7
‘QiuYeDx’ <qiuyedx@foxmail.com>
2023-07-31 22:55:46 +0800

    v1.8.0.3 Add MyProfileCard.jsx stagger渐入动画

--- src/modules/MyProfileCard/MyProfileCard.jsx
+++ src/modules/MyProfileCard/MyProfileCard.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import tw, {styled} from "twin.macro";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {solid} from "@fortawesome/fontawesome-svg-core/import.macro";
 import MainCard from "@/components/MainCard/MainCard";
 import {AvatarWrapper} from "@/components/Avatar/Styled.twin";
 import {WIDTH_MOBILE} from "@/styles/GlobalConfig";
+import gsap from "gsap";
 
 const SubAvatar = styled(AvatarWrapper)`
   position: absolute;
@@ -20,6 +21,26 @@ export default function MyProfileCard(props){
     const tw_onlyMobile = tw`bg-white md:col-span-3 col-span-5 m-4 relative  md:hidden mt-16`;
     const tw_notMobile = tw`bg-white md:col-span-3 col-span-5 m-4 relative hidden md:flex`;
     const tw_defaults = tw`bg-white md:col-span-3 col-span-5 m-4 relative`;
+
+    // 渐入动画
+    const gsap_ref = useRef(null);
+    useEffect(() => {
+        if(gsap_ref && gsap_ref.current === null){
+            gsap_ref.current = gsap.fromTo(".gsap_fade_in_up", {
+                y: 50,
+                opacity: 0,
+            }, {
+                y: 0,
+                opacity: 1,
+                duration: 1.5,
+                ease: 'power3.out',
+                repeat: 0,
+                stagger: 0.2,
+                delay: 0.2,
+            });
+        }
+    }, []);
+
     return (
         <MainCard _tw={props.onlyMobile ? tw_onlyMobile : (props.notMobile ? tw_notMobile : tw_defaults)} h={"360px"}>
             <br/>
@@ -50,17 +71,17 @@ export default function MyProfileCard(props){
             <div tw={"relative w-full pl-6 pr-6"}>
                 <div
                     tw={"pt-4 pb-4 text-xl text-white font-bold font-sans bg-blue-400 rounded-2xl flex justify-center items-center"}>
-                    <div tw={"flex-1 h-24 flex flex-col"}>
+                    <div className={'gsap_fade_in_up'} tw={"flex-1 h-24 flex flex-col"}>
                         <div tw={"flex-grow"}><FontAwesomeIcon icon={solid("mars")}/></div>
                         <div tw={"flex-grow font-normal text-base"}>性别</div>
                         <div tw={"flex-grow"}>男</div>
                     </div>
-                    <div tw={"flex-1 h-24 flex flex-col"}>
+                    <div className={'gsap_fade_in_up'} tw={"flex-1 h-24 flex flex-col"}>
                         <div tw={"flex-grow"}><FontAwesomeIcon icon={solid("user")}/></div>
                         <div tw={"flex-grow font-normal text-base"}>年龄</div>
                         <div tw={"flex-grow"}>21</div>
                     </div>
-                    <div tw={"flex-1 h-24 flex flex-col"}>
+                    <div className={'gsap_fade_in_up'} tw={"flex-1 h-24 flex flex-col"}>
                         <div tw={"flex-grow"}><FontAwesomeIcon icon={solid("graduation-cap")}/></div>
                         <div tw={"flex-grow font-normal text-base"}>学校</div>
                         <div tw={"flex-grow md:hidden"}>NEU</div>
